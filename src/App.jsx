8c23b050db7d9ce25ddc5360f3216f532ce53ef9
Elvis <elviskiplimo000@gmail.com>
2025-02-08 21:05:42 +0300

    not found 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar";
-import Footer from "./components/footer"; // Ensure Footer is imported
+import Footer from "./components/footer"; 
 import { Toaster } from "react-hot-toast";
 import { ToastContainer } from "react-toastify";
 import HomePage from "./pages/homepage";
@@ -13,25 +13,23 @@ const App = () => {
   return (
     <div
       style={{
-        overflowX:"hidden",
+        overflowX: "hidden",
         display: "flex",
         flexDirection: "column",
-        minHeight: "89vh", // Ensures the page fills the viewport
+        minHeight: "89vh",
       }}
     >
       <Router>
         <Navbar />
         <main style={{ flex: 1, padding: "20px" }}>
           <Routes>
-           
+            <Route path="/" element={<HomePage />} />
             <Route path="/updates" element={<Updates />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/signin" element={<Signin />} />
-
           </Routes>
         </main>
         <Footer />
-        <Routes> <Route path="/" element={<HomePage />} /></Routes>
         <Toaster />
         <ToastContainer />
       </Router>
